344e057b8ebd279ecaf3c7dfb63bb4a84a289b85
Peyton Thibodeaux <peytonthibodeaux@Peytons-MBP-2.attlocal.net>
2022-08-10 20:24:51 -0500

    now using chi router

--- exampleServer/main.go
+++ exampleServer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// setup of router
+	r := chi.NewRouter()
+
+	// basic middleware, good start
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// basic get call TODO: move out into it's own directory
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	// this port WILL be used in prod
+	http.ListenAndServe(":42069", r)
 }
