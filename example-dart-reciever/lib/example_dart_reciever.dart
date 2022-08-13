320bd83e65fbfa551b8a5e9816ded9bbe4acd6eb
Peyton Thibodeaux <peytonthibodeaux@Peytons-MBP-2.attlocal.net>
2022-08-13 01:34:38 -0500

    new dio options

--- example-dart-reciever/lib/example_dart_reciever.dart
+++ example-dart-reciever/lib/example_dart_reciever.dart
@@ -2,10 +2,16 @@
 import 'package:dio/dio.dart';
 
 void getPets() async {
-  // try {
-    var response = await Dio().get('http://localhost:42069/pets');
+  try {
+    var response = await Dio().get(
+      'http://localhost:42069/pets',
+      // options: Options(
+      //     //will not throw errors
+      //     validateStatus: (status) => true,
+      //   ),
+      );
     print(response);
-  // } catch (e) {
-  //   print(e);
-  // }
+  } catch (e) {
+    print(e);
+  }
 }
